4464444c0415c5feb000b7ff52a7f3616163e436
Jude Shamsi <judeshamsi@Judes-Laptop.local>
2024-10-21 15:05:15 +0100

    call spotify web sdk to successfully load and play music on app

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,166 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
-var SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID
-var SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET
+var DEV_SERVER = "http://localhost:5001";
+
+const track = {
+  name: "",
+  album: {
+      images: [
+          { url: "" }
+      ]
+  },
+  artists: [
+      { name: "" }
+  ]
+}
+
+function SpotifyWebPlayer ({imgUrl, token}) {
+
+  const [player, setPlayer] = useState(undefined);
+  const [is_paused, setPaused] = useState(false);
+  const [is_active, setActive] = useState(false);
+  const [current_track, setTrack] = useState(track);
+
+  useEffect(() => {
+
+    const script = document.createElement("script");
+    script.src = "https://sdk.scdn.co/spotify-player.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    window.onSpotifyWebPlaybackSDKReady = () => {
+
+        const player = new window.Spotify.Player({
+            name: 'Web Playback SDK',
+            getOAuthToken: cb => { cb(token); },
+            volume: 0.5
+        });
+
+        setPlayer(player);
+
+        player.addListener('ready', ({ device_id }) => {
+            console.log('Ready with Device ID', device_id);
+        });
+
+        player.addListener('not_ready', ({ device_id }) => {
+            console.log('Device ID has gone offline', device_id);
+        });
+
+        player.connect();
+
+        player.addListener('player_state_changed', ( state => {
+
+          if (!state) {
+              return;
+          }
+      
+          setTrack(state.track_window.current_track);
+          setPaused(state.paused);
+                
+          player.getCurrentState().then( state => { 
+              (!state)? setActive(false) : setActive(true) 
+          });
+      
+      }));
+
+    };
+
+}, [token]);
+
+  const handlePlay = () => {
+      if(player) {
+        player.togglePlay();
+        setPaused(!is_paused);
+      } else {
+        console.error("player not available")
+      }
+  }
+
+  return (
+    <>
+        <div className="container">
+            <div className="main-wrapper">
+                <img src={current_track.album.images[0].url} 
+                     className="now-playing__cover" alt="" />
+
+                <div className="now-playing__side">
+                    <div className="now-playing__name">{
+                                  current_track.name
+                                  }</div>
+
+                    <div className="now-playing__artist">{
+                                  current_track.artists[0].name
+                                  }</div>
+                </div>
+                <div>
+                <button className="btn-spotify" onClick={() => { player?.previousTrack() }} >
+                      &lt;&lt;
+                </button>
+
+                <button className="btn-spotify" onClick={handlePlay} >
+                    { is_paused ? "PLAY" : "PAUSE" }
+                </button>
+
+                <button className="btn-spotify" onClick={() => { player?.nextTrack() }} >
+                      &gt;&gt;
+                </button>
+                </div>
+            </div>
+        </div>
+     </>
+  )
+}
+
+function Login () {
+
+  return (
+    <a
+    className='App-login-button'
+    href={`${DEV_SERVER}/auth/login`}
+    target="_blank"
+    rel="noreferrer"
+    >Login
+    </a>
+  )
+}
 
 function App() {
+
+  const [authToken, setAuthToken] = useState(null);
+
+  useEffect(() => {
+
+    tokenAuthFlow();
+
+  }, [])
+
+  const tokenAuthFlow = async () => {
+    try {
+      const response = await fetch(DEV_SERVER + "/auth/token");
+      if(!response.ok) {
+        throw new Error(`authorization token failed, returned status code ${response.status}`);
+      }
+      const data = await response.json();
+      setAuthToken(data.access_token);
+    } catch(err) {
+      console.error(err)
+    }
+
+  }
+
+
+
   return (
-    <div className="App">
+    <div className="SpotifyWebPlayer">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        { !authToken ? <Login /> : <SpotifyWebPlayer token={authToken}/>}
       </header>
     </div>
   );
