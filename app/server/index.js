d92e9c6e38c9496abd435cb446897a0bd9857617
Jude Shamsi <judeshamsi@Judes-Laptop.local>
2024-10-21 14:33:40 +0100

    fixed oauth flow, successfully returning auth token

--- app/server/index.js
+++ app/server/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const generateRandomState = require("./utils").default;
+const cors = require('cors');
+const request = require('request');
+
+function generateRandomState (length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    for(var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+
+    return text;
+}
 
 const port = 5001;
 
@@ -10,57 +22,67 @@ var spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
 var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 
 var app = express();
+var access_token = '';
+
+app.use(cors({
+    origin: 'http://localhost:3000', // Replace with your frontend's URL
+    methods: ['GET', 'POST'], // Specify methods you want to allow
+}));
+
 
 app.get('/auth/login', (req, res) => {
-    let scope = "streaming \
-                    user-read-email \
-                        user-read-private"
 
-    let state = generateRandomState(16);
-
-    let auth_query_params = new URLSearchParams({
-        response_type: "code",
-        client_id: spotify_client_id,
-        scope: scope,
-        redirect_uri: "http://localhost:3000/auth/callback",
-        state: state
+    var scope = "streaming \
+                 user-read-email \
+                 user-read-private"
+  
+    var state = generateRandomState(16);
+  
+    var auth_query_parameters = new URLSearchParams({
+      response_type: "code",
+      client_id: spotify_client_id,
+      scope: scope,
+      redirect_uri: "http://localhost:5001/auth/callback",
+      state: state
     })
-
-    res.redirect("https://accounts.spotify.com/authorize/?" + auth_query_params)
-});
+  
+    res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString());
+  })
 
 app.get('/auth/callback', (req, res) => {
 
     var code = req.query.code;
-
     var authOptions = {
-        url: 'https://accounts.spotify.com/api/token',
-        form: {
-          code: code,
-          redirect_uri: "http://localhost:3000/auth/callback",
-          grant_type: 'authorization_code'
-        },
-        headers: {
-          'Authorization': 'Basic ' + (Buffer.from(spotify_client_id + ':' + spotify_client_secret).toString('base64')),
-          'Content-Type' : 'application/x-www-form-urlencoded'
-        },
-        json: true
-      };
-
-      request.post(authOptions, function(err, response, body) {
-        if(!err && response.statusCode === 200) {
-            var access_token = body.access_token;
-            res.redirect('/');
-        }
-      })
-
-});
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: "http://localhost:5001/auth/callback",
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (Buffer.from(spotify_client_id + ':' + spotify_client_secret).toString('base64')),
+        'Content-Type' : 'application/x-www-form-urlencoded'
+      },
+      json: true
+    };
+  
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        access_token = body.access_token;
+        res.redirect('http://localhost:3000')
+      }
+    });
+})
 
 app.get("/auth/token", (req, res) => {
-    res.json({
-        access_token: access_token
-    })
-})
+    if (access_token) {
+        res.json({
+            access_token: access_token
+        });
+    } else {
+        res.status(400).json({ error: 'Access token not found' });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
